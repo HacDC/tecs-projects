0eba267350bf478224dd06a329fd0cead32c2d1e
Ben Mendis <ben.mendis@gmail.com>
2012-12-05 11:17:15 -0500

    An implementation for Mult

--- project 04/mult/Mult.asm
+++ project 04/mult/Mult.asm
@@ -6,4 +6,22 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+    @R2
+    M=0  // initialize R2 to 0
+
+(LOOP)
+    @R1
+    D=M
+    @END
+    D;JEQ  // if R1 is 0, jump to end
+    @R1
+    M=M-1  // decrement R1
+    @R0
+    D=M
+    @R2
+    M=D+M  // R2 = R2 + R0
+    @LOOP
+    0;JMP  // restart the loop
+(END)
+    @END
+    0;JMP
