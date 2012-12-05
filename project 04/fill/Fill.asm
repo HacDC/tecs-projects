f6963caf6bd764d46a9500686299133af942a05d
Ben Mendis <ben.mendis@gmail.com>
2012-12-05 11:15:21 -0500

    An implementation for Fill

--- project 04/fill/Fill.asm
+++ project 04/fill/Fill.asm
@@ -8,4 +8,64 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-// Put your code here.
+    @16384
+    D=A
+    @screen_start
+    M=D
+    @position
+    M=D
+    @8191
+    D=D+A
+    @screen_end
+    M=D
+    @24576
+    D=A
+    @keyboard
+    M=D
+(LOOP)
+    @keyboard
+    A=M
+    D=M
+    @FILL
+    D;JNE
+  (CLEAR)
+    @position
+    A=M
+    D=M
+    @C_DEC
+    D;JNE
+    @position
+    D=M
+    @screen_start
+    D=D-M
+    @END
+    D;JEQ
+    @position
+    M=M-1
+   (C_DEC)
+    @position
+    A=M
+    M=!M
+    @END
+    0;JMP
+  (FILL)
+    @position
+    A=M
+    D=M
+    @F_INC
+    !D;JNE
+    @position
+    D=M
+    @screen_end
+    D=M-D
+    @END
+    D;JEQ
+    @position
+    M=M+1
+   (F_INC)
+    @position
+    A=M
+    M=!M
+(END)
+    @LOOP
+    0;JMP  // restart loop
